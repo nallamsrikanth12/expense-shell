9ed5c618251537b41f7be7397d6b43cdbd7fc020
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-24 17:36:06 +0630

    installation

--- backend.sh
+++ backend.sh
@@ -9,6 +9,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+echo "Please enter DB password:"
+read -s mysql_root_password
+
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
@@ -46,16 +49,33 @@ else
     echo -e "expense user already created.. $Y skipping $N"
 fi       
 
-mkdir -p /app
+mkdir -p /app &>>$LOGFILE
 VALIDATE $? "creating app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
 VALIDATE $? "download the backendcode"
 
-cd /app
-npm install
+cd /app &>>$LOGFILE
+npm install &>>$LOGFILE
 VALIDATE $? "install denpendencies"
 
+cp /home/ec2-user/expense-shell  /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "copied backend service"
 
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "daemon reload"
 
+systemctl start backend &>>$LOGFILE
+VALIDATE "start backend"
 
+systemctl enable backend &>>$LOGFILE
+VALIDATE "enable backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "install mysql client"
+
+mysql -h db.srikantheswar.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "scheme loading"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "restart backend"
