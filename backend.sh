4290871effc878f991be0f04b8dac0bb01f5410b
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-04-24 17:49:06 +0630

    installation

--- backend.sh
+++ backend.sh
@@ -62,7 +62,7 @@ VALIDATE $? "unzip backend code"
 npm install &>>$LOGFILE
 VALIDATE $? "install denpendencies"
 
-cp /home/ec2-user/expense-shell  /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shell/backend.service  /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
